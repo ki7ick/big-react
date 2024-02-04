38e78818e0303912556717c32031367a4f2ff326
KICKR <hehegaoxiang@163.com>
2024-02-04 15:29:28 +0800

    feat: jsx

---
+++ scripts/rollup/react.config.js
@@ -0,0 +1,53 @@
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+import {
+	getPackageJSON,
+	resolvePackagePath,
+	getBaseRollupPlugins
+} from './utils';
+
+const { name, module } = getPackageJSON('react');
+const pkgPath = resolvePackagePath(name);
+const pkgDistPath = resolvePackagePath(name, true);
+
+export default [
+	// react
+	{
+		input: `${pkgPath}/${module}`,
+		output: {
+			file: `${pkgDistPath}/index.js`,
+			name: 'index.js',
+			format: 'umd'
+		},
+		plugins: [
+			...getBaseRollupPlugins(),
+			generatePackageJson({
+				inputFolder: pkgPath,
+				outputFolder: pkgDistPath,
+				baseContents: ({ name, description, version }) => ({
+					name,
+					description,
+					version,
+					main: 'index.js'
+				})
+			})
+		]
+	},
+	// jsx-runtime
+	{
+		input: `${pkgPath}/src/jsx.ts`,
+		output: [
+			// jsx-runtime
+			{
+				file: `${pkgDistPath}/jsx-runtime.js`,
+				name: 'jsx-runtime.js',
+				format: 'umd'
+			},
+			{
+				file: `${pkgDistPath}/jsx-dev-runtime.js`,
+				name: 'jsx-dev-runtime.js',
+				format: 'umd'
+			}
+		],
+		plugins: getBaseRollupPlugins()
+	}
+];
